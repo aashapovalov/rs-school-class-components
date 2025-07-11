0243077a364059462387fe6edec5ea270ef28198
Aleksei Shapovalov <aleksei.a.shapovalov@gmail.com>
2025-07-11 23:52:37 +0300

    feat: add ErrorBoundary component and integrate it in main app

---
+++ src/ErrorBoundary.tsx
@@ -0,0 +1,35 @@
+import * as React from 'react';
+
+interface State {
+    hasError: boolean;
+    error: Error | null;
+}
+
+export default class ErrorBoundary extends React.Component {
+    state: State = { 
+        hasError: false, 
+        error: null
+    } 
+
+
+  static getDerivedStateFromError(error: Error): State {
+    return {hasError: true, error };
+  }
+
+   componentDidCatch(error: Error, errorInfo: React.ErrorInfo) {
+    console.error("Error caught by ErrorBoundary:", error, errorInfo);
+  }
+
+  render() {
+    if (this.state.hasError) {
+  return (
+        <>
+        <div className='error-container'>
+            <p className='error-name'>Error: {this.state.error?.name}</p>
+            <p className='error-message'>{this.state.error?.message ?? 'Something went wrong'}</p>
+        </div>
+        </>
+    )
+  }
+}
+}
