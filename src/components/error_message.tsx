390c1d01c4f700e1da0a8e28bd774dde0cf001dc
Aleksei Shapovalov <aleksei.a.shapovalov@gmail.com>
2025-07-23 17:40:14 +0300

    feat: refactor Error Message component from class to functions

--- src/components/error_message.tsx
+++ src/components/error_message.tsx
@@ -1,28 +1,21 @@
-import { Component } from 'react';
-
 import noResultsImg from '../assets/no_results_error.png';
 
-import type { ErrorMessageProps } from './types';
+export default function ErrorMessage({ message }: { message: string }) {
+  const isNotFound = message.toLowerCase().includes('there is nothing here');
 
-export default class ErrorMessage extends Component<ErrorMessageProps> {
-  render() {
-    const { message } = this.props;
-    const isNotFound = message.toLowerCase().includes('there is nothing here');
-
-    return (
-      <div className="error-message">
-        {isNotFound ? (
-          <img
-            src={noResultsImg}
-            alt="No Results Error"
-            className="no-results-error"
-          />
-        ) : (
-          <p className="general-error">
-            Error: <span>{message}</span>
-          </p>
-        )}
-      </div>
-    );
-  }
+  return (
+    <div className="error-message">
+      {isNotFound ? (
+        <img
+          src={noResultsImg}
+          alt="No Results Error"
+          className="no-results-error"
+        />
+      ) : (
+        <p className="general-error">
+          Error: <span>{message}</span>
+        </p>
+      )}
+    </div>
+  );
 }
