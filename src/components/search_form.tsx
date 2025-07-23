8323caa77a9da3ac207c5398cacbaf1d343b7e8b
Aleksei Shapovalov <aleksei.a.shapovalov@gmail.com>
2025-07-23 21:55:48 +0300

    feat: refactor Search Form component from class to functions

--- src/components/search_form.tsx
+++ src/components/search_form.tsx
@@ -1,4 +1,3 @@
-import { Component } from 'react';
 import { CrashButton } from './';
 
 import type { SearchFormProps } from './types';
@@ -6,43 +5,39 @@ import type { SearchFormProps } from './types';
 import deviceImg from '../assets/search_device_desktop_no_background.png';
 import appLogo from '../assets/app_logo.png';
 
-export default class SearchForm extends Component<SearchFormProps> {
-  render() {
-    return (
-      <>
-        <section className="device">
-          <img src={appLogo} className="logo" />
-          <h1 className="character-search">Character Search</h1>
+export default function SearchForm(props: SearchFormProps) {
+  return (
+    <section className="device">
+      <img src={appLogo} className="logo" />
+      <h1 className="character-search">Character Search</h1>
 
-          <div className="device-frame">
-            <img src={deviceImg} alt="Device" className="device-no-bg" />
-            <form
-              onSubmit={(e) => {
-                e.preventDefault();
-                this.props.onSearchSubmit(this.props.inputValue);
-              }}
-            >
-              <input
-                className="search-input"
-                value={this.props.inputValue}
-                onChange={this.props.onInputChange}
-                placeholder="Search for a character..."
-                type="text"
-              />
-              <button
-                onClick={(e) => {
-                  e.preventDefault();
-                  this.props.onSearchSubmit(this.props.inputValue);
-                }}
-                className="search-btn"
-                type="submit"
-                aria-label="Search"
-              />
-            </form>
-            <CrashButton />
-          </div>
-        </section>
-      </>
-    );
-  }
+      <div className="device-frame">
+        <img src={deviceImg} alt="Device" className="device-no-bg" />
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            props.onSearchSubmit(props.inputValue);
+          }}
+        >
+          <input
+            className="search-input"
+            value={props.inputValue}
+            onChange={props.onInputChange}
+            placeholder="Search for a character..."
+            type="text"
+          />
+          <button
+            onClick={(e) => {
+              e.preventDefault();
+              props.onSearchSubmit(props.inputValue);
+            }}
+            className="search-btn"
+            type="submit"
+            aria-label="Search"
+          />
+        </form>
+        <CrashButton />
+      </div>
+    </section>
+  );
 }
