5b18b30fe176f9af0c9a81483e2d604f5ce163ff
Aleksei Shapovalov <aleksei.a.shapovalov@gmail.com>
2025-07-24 10:27:56 +0300

    feat: refactor App component from class to functions

--- src/app.tsx
+++ src/app.tsx
@@ -1,107 +1,92 @@
-import { Component } from 'react';
+import { useEffect, useState } from 'react';
 import { SearchForm, ResultsList, Spinner, ErrorMessage } from './components';
-import type { AppState, AppProps } from './types';
 
 import './app.css';
 import genErrorMortyImg from './assets/general_error_morty.png';
 
-export default class App extends Component {
-  state: AppState = {
-    inputValue: '',
-    searchResults: [],
-    loading: false,
-    error: null,
-  };
+export default function App() {
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+  const [searchResults, setSearchResults] = useState([]);
+  const [inputValue, setInputValue] = useState('');
 
-  constructor(props: AppProps) {
-    super(props);
-    this.handleInputChange = this.handleInputChange.bind(this);
-    this.nameTransform = this.nameTransform.bind(this);
-    this.handleSearchSubmit = this.handleSearchSubmit.bind(this);
-  }
+  useEffect(() => {
+    const savedQuary = localStorage.getItem('searchQuery') || '';
+    setInputValue(savedQuary);
+    handleSearchSubmit(savedQuary);
+  }, []);
 
-  handleInputChange(e: React.ChangeEvent<HTMLInputElement>) {
-    this.setState({ inputValue: e.target.value });
-  }
-
-  nameTransform(name: string) {
+  function nameTransform(name: string) {
     return name.toLowerCase().replace(/\s+/g, '%20');
   }
 
-  async handleSearchSubmit(input: string) {
-    this.setState({ loading: true, error: null });
+  function handleInputChange(e: React.ChangeEvent<HTMLInputElement>) {
+    setInputValue(e.target.value);
+  }
+
+  async function handleSearchSubmit(input: string) {
     localStorage.removeItem('searchQuery');
     localStorage.setItem('searchQuery', input.trim());
-    const searchTerm: string = input;
+
     const urlBase: string = 'https://rickandmortyapi.com/api/character';
     try {
-      if (searchTerm.trim() === '') {
+      if (input.trim() === '') {
         const responseAll = await fetch(urlBase);
         if (!responseAll.ok) {
           throw new Error('Network response was not ok');
         }
         const data = await responseAll.json();
-        this.setState({ searchResults: data.results, loading: false });
+        setSearchResults(data.results);
+        setLoading(false);
       } else {
-        const urlName: string = `${urlBase}/?name=${this.nameTransform(searchTerm.trim())}`;
+        const urlName: string = `${urlBase}/?name=${nameTransform(input.trim())}`;
         const response = await fetch(urlName);
         const data = await response.json();
         if (!response.ok) {
-          this.setState({
-            searchResults: [],
-            loading: false,
-            error: data.error || 'Unknown error',
-          });
+          setSearchResults([]);
+          setLoading(false);
+          setError(data.error || 'Unknown error');
           return;
         }
-        this.setState({ searchResults: data.results, loading: false });
+        setSearchResults(data.results);
+        setLoading(false);
       }
     } catch (error) {
       if (error instanceof Error) {
-        this.setState({ error: error.message, loading: false });
+        setError(error.message);
+        setLoading(false);
       } else {
-        this.setState({ error: 'Unknown error', loading: false });
+        setError('Unknown error');
+        setLoading(false);
       }
     }
   }
 
-  componentDidMount() {
-    console.log('App component Did Mount');
-    const inputValue = localStorage.getItem('searchQuery') || '';
-    this.setState({ inputValue }, () => {
-      this.handleSearchSubmit(inputValue);
-    });
-  }
-
-  render() {
-    console.log('Rendering SearchForm');
-    const { loading } = this.state;
-    return (
-      <div className="app">
-        {loading ? <Spinner /> : null}
-        <div className={this.state.loading ? 'blurring' : ''}>
-          <SearchForm
-            inputValue={this.state.inputValue}
-            onInputChange={this.handleInputChange}
-            onSearchSubmit={this.handleSearchSubmit}
-          />
-        </div>
-        {this.state.error ? (
-          this.state.error === 'There is nothing here' ? (
-            <ErrorMessage message={this.state.error} />
-          ) : (
-            <div className="error-block">
-              <img
-                src={genErrorMortyImg}
-                alt="Morty panic"
-                className="general-error-morty"
-              />
-              <ErrorMessage message={this.state.error} />
-            </div>
-          )
-        ) : null}
-        <ResultsList results={this.state.searchResults} />
+  return (
+    <div className="app">
+      {loading ? <Spinner /> : null}
+      <div className={loading ? 'blurring' : ''}>
+        <SearchForm
+          inputValue={inputValue}
+          onInputChange={handleInputChange}
+          onSearchSubmit={handleSearchSubmit}
+        />
       </div>
-    );
-  }
+      {error ? (
+        error === 'There is nothing here' ? (
+          <ErrorMessage message={error} />
+        ) : (
+          <div className="error-block">
+            <img
+              src={genErrorMortyImg}
+              alt="Morty panic"
+              className="general-error-morty"
+            />
+            <ErrorMessage message={error} />
+          </div>
+        )
+      ) : null}
+      <ResultsList results={searchResults} />
+    </div>
+  );
 }
