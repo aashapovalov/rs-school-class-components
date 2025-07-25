6db421ee27e35bd525e39f92d129f9130705d95d
Aleksei Shapovalov <aleksei.a.shapovalov@gmail.com>
2025-07-25 22:20:27 +0300

    feat: add pagination styling

--- src/components/results_list.tsx
+++ src/components/results_list.tsx
@@ -7,24 +7,26 @@ export default function ResultsList(props: ResultsListProps) {
   const { results } = props;
 
   return (
-    <section className="result-section">
-      {results.map((character, index) => (
-        <div
-          key={index}
-          className={`character-card ${index}`}
-          data-testid="character-card"
-        >
-          <img
-            className="character-image"
-            src={character.image ? character.image : fallbackImg}
-            alt={character.name}
-          />
-          <div className="character-description">
-            <h2 className="character-name">{character.name}</h2>
+    <>
+      <section className="result-section">
+        {results.map((character, index) => (
+          <div
+            key={index}
+            className={`character-card ${index}`}
+            data-testid="character-card"
+          >
+            <img
+              className="character-image"
+              src={character.image ? character.image : fallbackImg}
+              alt={character.name}
+            />
+            <div className="character-description">
+              <h2 className="character-name">{character.name}</h2>
+            </div>
           </div>
-        </div>
-      ))}
+        ))}
+      </section>
       <PagesList {...props} />
-    </section>
+    </>
   );
 }
