69c3ca7b74a99b4ab7e84f4e1d4712981933399e
Aleksei Shapovalov <aleksei.a.shapovalov@gmail.com>
2025-07-26 11:04:35 +0300

    feat: change crash button for about button and provide navigation

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,7 +1,7 @@
 export { default as ResultsList } from './results_list';
 export { default as SearchForm } from './search_form';
 export { default as ErrorMessage } from './error_message';
-export { default as CrashButton } from './crash_button';
+export { default as CrashButton } from './about_button';
 export { default as Spinner } from './spinner';
 export { default as AboutPage } from './about_page';
 export { useLocalStorage } from './use_local_storage';
