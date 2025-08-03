102d7a4ae7a11c8992ec06cc75fb3921c6be75fc
Aleksei Shapovalov <aleksei.a.shapovalov@gmail.com>
2025-08-03 20:43:25 +0300

    feat: add tests for blob and download link creation

--- src/_test-utils/select_modal.test.tsx
+++ src/_test-utils/select_modal.test.tsx
@@ -9,6 +9,17 @@ afterEach(() => {
   useStore.setState({ selectedCharacters: [] });
 });
 
+let originalCreateObjectURL: typeof URL.createObjectURL;
+
+beforeAll(() => {
+  originalCreateObjectURL = URL.createObjectURL;
+  URL.createObjectURL = vi.fn(() => 'blob:mock-url');
+});
+
+afterAll(() => {
+  URL.createObjectURL = originalCreateObjectURL;
+});
+
 test('renders SelectModal correctly', () => {
   render(<SelectModal />);
   const DownloadBtn = screen.getByRole('button', { name: /download/i });
@@ -62,4 +73,35 @@ test('clears state on unselect button click', async () => {
   await userEvent.click(UnselectBtn);
 
   expect(useStore.getState().selectedCharacters).toEqual([]);
+});
+
+test('calls DownloadArray with selected characters on download button click', async () => {
+  const spy = vi
+    .spyOn(global.URL, 'createObjectURL')
+    .mockReturnValue('blob:mock-url');
+
+  const clickMock = vi.fn();
+
+  vi.spyOn(document, 'createElement').mockImplementation((tagName: string) => {
+    const element = document.createElementNS(
+      'http://www.w3.org/1999/xhtml',
+      tagName
+    );
+    if (tagName === 'a') {
+      element.click = clickMock;
+    }
+    return element;
+  });
+
+  act(() => {
+    useStore.setState({ selectedCharacters: mockSelectedCharactersTwo });
+  });
+
+  render(<SelectModal />);
+
+  const downloadBtn = screen.getByRole('button', { name: /download/i });
+  await userEvent.click(downloadBtn);
+
+  expect(spy).toHaveBeenCalled(); // confirm CSV blob was created
+  expect(clickMock).toHaveBeenCalled(); // confirm download link was clicked
 });
