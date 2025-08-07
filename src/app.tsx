902a1049c3bf6c3f7e4ed5a7527eb6a6d70cfe45
Aleksei Shapovalov <aleksei.a.shapovalov@gmail.com>
2025-08-07 20:55:09 +0300

    feat: wrap app component in QueryClientProvider

--- src/app.tsx
+++ src/app.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Route, Routes } from 'react-router';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 import { SearchRequest, SearchLayout } from '@/entities';
 import { NotFoundPage, AboutPage } from '@/pages';
@@ -6,25 +7,29 @@ import { ThemeProvider } from '@/state/context';
 
 import './app.css';
 
+const queryClient = new QueryClient();
+
 export function App() {
   return (
     <>
-      <ThemeProvider>
-        <div className="bg-[var(--Background-light)] text-[var(--Font-color-basic)] dark:bg-[var(--Background-dark)] dark:text-[var(--Font-color-basic-dark)] min-h-screen w-full">
-          <div className="max-w-[102.4rem] mx-auto p-4">
-            <BrowserRouter>
-              <Routes>
-                <Route path="/" element={<SearchLayout />}>
-                  <Route index element={<SearchRequest />} />
-                </Route>
+      <QueryClientProvider client={queryClient}>
+        <ThemeProvider>
+          <div className="bg-[var(--Background-light)] text-[var(--Font-color-basic)] dark:bg-[var(--Background-dark)] dark:text-[var(--Font-color-basic-dark)] min-h-screen w-full">
+            <div className="max-w-[102.4rem] mx-auto p-4">
+              <BrowserRouter>
+                <Routes>
+                  <Route path="/" element={<SearchLayout />}>
+                    <Route index element={<SearchRequest />} />
+                  </Route>
 
-                <Route path="/about" element={<AboutPage />} />
-                <Route path="*" element={<NotFoundPage />} />
-              </Routes>
-            </BrowserRouter>
+                  <Route path="/about" element={<AboutPage />} />
+                  <Route path="*" element={<NotFoundPage />} />
+                </Routes>
+              </BrowserRouter>
+            </div>
           </div>
-        </div>
-      </ThemeProvider>
+        </ThemeProvider>
+      </QueryClientProvider>
     </>
   );
 }
